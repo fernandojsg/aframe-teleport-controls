1fd4c67655ef43f4c3bcaeb586035860e752a03e
Fernando Serrano <fernandojsg@gmail.com>
2016-10-29 13:15:21 +0200

    Replace ghpages with gh-pages

--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 var webpack = require('webpack');
 
+var outputFilename = 'build.js';
+
 var PLUGINS = [];
 if (process.env.NODE_ENV === 'production') {
   PLUGINS.push(new webpack.optimize.UglifyJsPlugin());
+  outputFilename = 'examples/build.js';
 }
 
 module.exports = {
   entry: './examples/main.js',
   output: {
     path: __dirname,
-    filename: 'build.js'
+    filename: outputFilename
   },
   plugins: PLUGINS
 };
